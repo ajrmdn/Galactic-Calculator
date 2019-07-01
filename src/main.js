1c4972e3c1402c78adeb9bbe657ded7df6c77195
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-30 23:06:15 -0700

    add edits to my README

--- src/main.js
+++ src/main.js
@@ -6,15 +6,5 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function () {
-
  $('#galacticYears').submit(function (event) {
    event.preventDefault();
-   const userDeath = $('#demoAgeInput').val();
-
-
-   const yearsToLive = userDeath  ;
-   const deathAge = new DoomsDay(yearsToLive);
-
-   $('.output').text(deathAge.returnYearsLeft());
- });
-});
